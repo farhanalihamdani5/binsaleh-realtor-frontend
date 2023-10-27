0b7204c9c489ab4312b90bcad3c6553265787ef3
District3developers <farhanalihamdani2698@outlook.com>
2023-10-27 07:42:56 +0500

    continue commit

--- README.md
+++
@@ -1,9 +0,0 @@
-# binsaleh-realtor-frontend
-binsaleh real estate to roy usama manzoor
-
-ecommerce
-sanity 
-mongodb 
-next 
-tailwind post css
-102623 
--- ecommerce-app/app/api/checkout/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server"
-// @ts-ignore
-import { validateCartItems } from "use-shopping-cart/utilities"
-
-import { inventory } from "@/config/inventory"
-import { stripe } from "@/lib/stripe"
-
-export async function POST() {}
