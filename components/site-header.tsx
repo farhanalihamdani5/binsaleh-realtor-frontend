f75e0a2e8a4122e2535a85398c40aceb2a11dccb
District3developers <farhanalihamdani2698@outlook.com>
2023-10-28 05:58:31 +0500

    dev initial

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link"
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { Edit, ShoppingBag } from "lucide-react"
+import { Studio } from "sanity"
 import { useShoppingCart } from "use-shopping-cart"
 
 import { Button } from "@/components/ui/button"
@@ -11,6 +12,12 @@ import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
+
+  const pathName =usePathname();
+  if (pathName.startsWith('/studio')) {
+    return null
+  }
+
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="mx-auto flex h-16 max-w-6xl items-center justify-between space-x-4 px-6 sm:space-x-0">
@@ -34,6 +41,13 @@ export function SiteHeader() {
             </Button>
           </Link>
           <ThemeToggle />
+          {process.env.NODE_ENV === "development" && (
+            <Link href="/studio">
+              <Button size="sm" variant="ghost">
+                <Edit className="h-5 w-5" />
+              </Button>
+            </Link>
+          )}
         </div>
       </div>
     </header>
