89d9579bd9bea6630728abf9024c58291ac2e2bb
farhanalihamdani5 <farhanalihamdani2698@outlook.com>
2023-11-20 12:59:14 +0500

    ok

--- components/product-grid.tsx
+++ components/product-grid.tsx
@@ -9,9 +9,11 @@ import { formatCurrencyString } from "use-shopping-cart"
 import { SanityProduct } from "@/config/inventory"
 import { shimmer, toBase64 } from "@/lib/image"
 
-interface Props {}
+interface Props {
+  products: SanityProduct[]
+}
 
-export function ProductGrid() {
+export function ProductGrid({ products }: Props) {
   if ([].length === 0) {
     return (
       <div className="mx-auto grid h-40 w-full place-items-center rounded-md border-2 border-dashed bg-gray-50 py-10 text-center dark:bg-gray-900">
@@ -27,19 +29,19 @@ export function ProductGrid() {
 
   return (
     <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-3 lg:col-span-3 lg:gap-x-8">
-      {[].map((product) => (
-        <Link key={"key"} href={`/products/slug`} className="group text-sm">
+      {products.map((product) => (
+        <Link key={product._id} href={`/products/${product.slug}`} className="group text-sm">
           <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg border-2 border-gray-200 bg-gray-100 group-hover:opacity-75 dark:border-gray-800">
             <Image
-              src={"src"}
+              src={urlForImage(product.images[0]).url()}
               alt={"name"}
-              width={0}
-              height={0}
+              width={225}
+              height={280}
               className="h-full w-full object-cover object-center"
             />
           </div>
-          <h3 className="mt-4 font-medium">Name</h3>
-          <p className="mt-2 font-medium">Price</p>
+          <h3 className="mt-4 font-medium">{product.name}</h3>
+          <p className="mt-2 font-medium">{product.price}</p>
         </Link>
       ))}
     </div>
